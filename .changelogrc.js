e6f682fb6da5ffc9edf6b3dcd9e6fe33d57d257c
xzl_2021 <zilong@ftsafe.com>
2024-01-30 19:11:46 +0800

    Hello

---
+++ .changelogrc.js
@@ -0,0 +1,8 @@
+/**
+ * @author        xzl_2021 <zilong@ftsafe.com>
+ * @date          2024-01-30 19:11:00
+ * Copyright © YourCompanyName All rights reserved
+ */
+module.exports = {
+  titleLanguage: 'zh-CN',
+}
---
+++ .commitlintrc.js
@@ -0,0 +1,8 @@
+/**
+ * @author        xzl_2021 <zilong@ftsafe.com>
+ * @date          2024-01-30 19:10:45
+ * Copyright © YourCompanyName All rights reserved
+ */
+module.exports = {
+  extends: ['gitmoji'],
+}
